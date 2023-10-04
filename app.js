dbcc591fb910d27439937bf5adcbc0623120f807
Sandeep <sandeepsoni@deccansoft.com>
2023-10-04 16:09:10 +0530

    A

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // app.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.get('/', (req, res) => {
   res.send('Hello, this is my web application!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at ${port}`);
 });
